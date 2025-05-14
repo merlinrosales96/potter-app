621f09e93d119e4a797dc164fcab461481634fe5
Merlin Rosales <merlinrosales96@gmail.com>
2025-05-14 16:25:23 -0500

    Change: Normal color

--- src/utils/Utils.tsx
+++ src/utils/Utils.tsx
@@ -2,7 +2,7 @@ export const itemsPerPage = 20;
 
 
 export const typeColors: { [key: string]: string } = {
-    normal: '#FFFFFF',
+    normal: '#E8C07C',
     gryffindor: '#7F0909',
     slytherin: '#1A472A',
     ravenclaw: '#0E1A40',
